2caa89e8a3c9c6565a1239c53546c3d81ecd2d38
anstcher <anastasiacher73@gmail.com>
2023-02-01 13:57:53 +0100

    Rutas agregadas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hola mundo!')
 });
+
+router.get('/employees', function(req, res, next) {
+  res.send('Obteniendo empleados')
+});
+
+router.post('/employees', function(req, res, next) {
+  res.send('Creando')
+});
+
+router.put('/employees', function(req, res, next) {
+  res.send('Actualizando empleado')
+});
+
+router.delete('/employees', function(req, res, next) {
+  res.send('Eliminano empleados')
+});
+
+
 
 module.exports = router;
